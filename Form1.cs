8dc4f6a03d6b80181aabaf9289df4d7370bda2e6
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-11-19 16:16:08 +0000

    Adicionar arquivos de projeto.

---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace nac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        dsemana cunha = new dsemana();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            cunha.recebe = textBox1.Text;
+            label1.Text = cunha.envia;
+            label1.Text = ("O dia da semana em que você nasceu é " + cunha.envia);
+        }
+    }
+}
